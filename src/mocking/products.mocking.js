c54db422d9900025a49ac6796e3901692fed904e
Your Name <you@example.com>
2023-04-04 18:12:51 -0300

    agregue mocks y manejo de erroes

---
+++ src/mocking/products.mocking.js
@@ -0,0 +1,65 @@
+import { Router } from "express";
+import { generateProducts } from "../../utils.js";
+import CustomError from "../services/errors/CustomError.js";
+import EErrors from "../services/errors/enums.js";
+import {
+  generatePropertyError,
+  generateDuplicatedError,
+  generateStockError,
+} from "../services/errors/info.js";
+
+const router = Router();
+const product = [];
+
+router.get("/", (req, res) => {
+  for (let i = 0; i < 100; i++) {
+    product.push(generateProducts());
+  }
+  res.json({ status: "success", payload: products });
+});
+
+router.post("/", (req, res) => {
+  const {
+    id,
+    title,
+    description,
+    code,
+    price,
+    stock,
+    status,
+    category,
+    thumbnails,
+  } = req.body;
+  const duplicated = products.find((p) => p.code === code);
+
+  if (
+    !title ||
+    !code ||
+    !price ||
+    !stock ||
+    typeof title !== "string" ||
+    typeof code !== "string" ||
+    typeof price !== "number" ||
+    typeof stock !== "number"
+  ) {
+    CustomError.createError({
+      name: "Product creation error",
+      cause: generatePropertyError({ title, code, price, stock }),
+      message: "Error trying to create product",
+      code: EErrors.INVALID_TYPES_ERROR,
+    });
+  }else if (typeof(stock) !=="number"||stock<0){
+    CustomError.createError({
+        name: "Product creation error",
+        cause: generateStockError(stock),
+        message: "Error trying to create product",
+        code: EErrors.STOCK_ERROR
+    })
+  }
+
+  const product = {id,title,description,code,price,stock,status,category,thumbnails}
+  product.push(product)
+    res.json({status:"success",payload:product})
+});
+
+export default router
